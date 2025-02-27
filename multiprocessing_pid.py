df87a9befe86c4d0fd408e68f148646b4908ecbe
markophir <90481708+Alkimik9000@users.noreply.github.com>
2025-02-27 11:08:45 +0200

    Update multiprocessing_pid.py

--- multiprocessing_pid.py
+++ multiprocessing_pid.py
@@ -2,15 +2,17 @@ import os
 import multiprocessing
 
 def childProcess(i):
-    print("Child PID is " + str(os.getpid()) + " and its parent PID is " + str(os.getppid()))
-    print("Grandparent PID is " + str(os.getppid()))
-    print(i)
+    print("Child PID is " + str(os.getpid()) + " and its parent PID is " + str(os.getppid()) + " | Value: " + str(i))
 
 def main():
     number = int(input("Enter a number: "))
+    processes = []
     for i in range(number + 1):
         process = multiprocessing.Process(target=childProcess, args=(i,))
         process.start()
+        processes.append(process)
+
+    for process in processes:
         process.join()
 
     print("Parent PID is " + str(os.getpid()) + " and its parent PID is " + str(os.getppid()))
