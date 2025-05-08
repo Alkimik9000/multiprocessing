656fc0fd992b14b467f2e685e458f3f58404c5c4
markophir <90481708+Alkimik9000@users.noreply.github.com>
2025-05-08 15:45:26 +0300

    Store thread index with duration and print sorted results
    Updated the downloader to append a tuple of (index, duration) instead of just the duration. After all threads complete, the program sorts the list and prints each thread’s index.

--- multithread_json_downloader.py
+++ multithread_json_downloader.py
@@ -3,11 +3,12 @@ import time
 import requests
 import threading
 
-def downloader(url, thread_durations):
+def downloader(i, url, thread_durations):
     thread_start_time = time.time()
     requests.get(url).json()
     thread_end_time = time.time()
-    thread_durations.append(thread_end_time - thread_start_time)
+    thread_durations.append((i, thread_end_time - thread_start_time))
+    # thread_durations.append(thread_end_time - thread_start_time)
 
 def main():
     urls = [
@@ -21,16 +22,16 @@ def main():
 
     threads = []
     thread_durations = []
-    for url in urls:
-        thread = threading.Thread(target=downloader, args=(url, thread_durations))
+    for i, url in enumerate(urls):
+        thread = threading.Thread(target=downloader, args=(i, url, thread_durations))
         thread.start()
         threads.append(thread)
 
     for thread in threads:
         thread.join()
 
-    for i, duration in enumerate(thread_durations):
-        print("Thread " + str(i) + " took {:.2f} seconds to download its content".format(duration))
+    for i, duration in sorted(thread_durations):
+        print("Thread " + str(i + 1)+ " " + str(urls[i]) + " took {:.2f} seconds to download its content".format(duration))
 
 if __name__ == "__main__":
     main()
