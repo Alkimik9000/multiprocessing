5bc4df9c8b6d600d04e038de0e083747aca9796e
markophir <90481708+Alkimik9000@users.noreply.github.com>
2025-05-08 13:22:04 +0300

    Add print before joining to clarify process flow
    Added a new print statement after starting the child process and before calling join() to make it clearer when the parent is waiting. The code also now uses a single child process that loops and fills the queue, but the main change is improving the output for better tracking.

--- multiprocessing_sum_collected_values.py
+++ multiprocessing_sum_collected_values.py
@@ -1,28 +1,26 @@
+
 import os
 import multiprocessing
 import time
 
-def childProcess(i, results_queue):
-    print("Child PID is " + str(os.getpid()) + " and its parent PID is " + str(os.getppid()) + " | Value: " + str(i))
-    results_queue.put(i)
+def childProcess(number, results_queue):
+    print("Child PID is " + str(os.getpid()) + " and its parent PID is " + str(os.getppid()))
+    for i in range(number + 1):
+        print("Child Process is printing: " + str(i) + " out of " + str(number))
+        results_queue.put(i)
 
 def main():
     results_queue = multiprocessing.Queue()
     number = int(input("Enter a number: "))
-    processes = []
-    for i in range(number + 1):
-        process = multiprocessing.Process(target=childProcess, args=(i, results_queue, ))
-        process.start()
-        time.sleep(0.15)
-        print("Randomly Inserted Line After Start While Waiting 0.15 Seconds")
-    
-        processes.append(process)
-
-    for process in processes:
-        process.join()
+    print("Parent PID is " + str(os.getpid()) + " and its parent PID is " + str(os.getppid()))
+    process = multiprocessing.Process(target=childProcess, args=(number, results_queue))
+    process.start()
+    time.sleep(0.15)
+    print("Randomly Inserted Line After Start and Beofre Joining")
+    process.join()
 
     accumulated_sum = 0
-    for x in range(i + 1):
+    for x in range(number + 1):
         accumulated_sum += results_queue.get()
     print(accumulated_sum)
 
